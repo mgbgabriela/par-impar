f1d04ad18bdf5da2fad0bbb5a117e8f23c67692f
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-21 21:02:27 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/*Ejercicio – Par/Impar
+• Desarrollar un algoritmo que dado un número,
+ingresado por el usuario determine si el
+número es par o impar y le informe al usuario
+• En el caso de ser 0 (cero) el algoritmo deberá
+informarlo*/
+
 let dato = document.getElementById("dato");
 let btnEnviar = document.getElementById("enviar");
 
